39f90176e12548bf243acc328fac9af8350472fd
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-14 10:00:29 +0530

    Address test using rspec

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  
+  it "line1 should be present" do
+    address = build(:address, line1: "")
+    expect(address).to be_valid
+  end
+
+  it "line1 should be present" do
+    address = build(:address, line1: "a" * 51)
+    expect(address).not_to be_valid
+  end
+
+  it "line2 should be present" do
+    address = build(:address, line2: "a" * 51)
+    expect(address).not_to be_valid
+  end
+
+  it "city_village should be present" do
+    address = build(:address, city_village: "")
+    expect(address).to be_valid
+  end
+
+  it "city_village should be present" do
+    address = build(:address, city_village: "a" * 51)
+    expect(address).not_to be_valid
+  end
+
+  it "district should be present" do
+    address = build(:address, district: "")
+    expect(address).to be_valid
+  end
+
+  it "district should be present" do
+    address = build(:address, district: "a" * 51)
+    expect(address).not_to be_valid
+  end
+
+  it "state should be present" do
+    address = build(:address, state: "")
+    expect(address).to be_valid
+  end
+
+  it "state should be present" do
+    address = build(:address, state: "a" * 51)
+    expect(address).not_to be_valid
+  end
+
+  it "country should be present" do
+    address = build(:address, country: "")
+    expect(address).to be_valid
+  end
+
+  it "country should be present" do
+    address = build(:address, country: "a" * 51)
+    expect(address).not_to be_valid
+  end
 end
--- spec/models/personal_spec.rb
+++ spec/models/personal_spec.rb
@@ -3,52 +3,52 @@ require 'rails_helper'
 RSpec.describe Personal, type: :model do
 
   it "first name should be present" do
-    personal = FactoryGirl.build(:personal, first_name: '')
+    personal = build(:personal, first_name: '')
     expect(personal).not_to be_valid
   end
 
   it "first name should not be too long" do
-    personal = FactoryGirl.build(:personal, first_name: 'a' * 21)
+    personal = build(:personal, first_name: 'a' * 21)
     expect(personal).not_to be_valid
   end
 
   it "middle name should not be too long" do
-    personal = FactoryGirl.build(:personal, middle_name: 'a' * 21)
+    personal = build(:personal, middle_name: 'a' * 21)
     expect(personal).not_to be_valid
   end
 
   it "last name should be present" do
-    personal = FactoryGirl.build(:personal, last_name: '')
+    personal = build(:personal, last_name: '')
     expect(personal).not_to be_valid
   end
 
   it "last name should not be too long" do
-    personal = FactoryGirl.build(:personal, last_name: 'a' * 21)
+    personal = build(:personal, last_name: 'a' * 21)
     expect(personal).not_to be_valid
   end
 
   it "date of birth should be present" do
-    personal = FactoryGirl.build(:personal, date_of_birth: "")
+    personal = build(:personal, date_of_birth: "")
     expect(personal).not_to be_valid
   end
 
   it "mothers name should be present" do
-    personal = FactoryGirl.build(:personal, mothers_name: '')
+    personal = build(:personal, mothers_name: '')
     expect(personal).not_to be_valid
   end
 
   it "mothers name should not be too long" do
-    personal = FactoryGirl.build(:personal, mothers_name: 'a' * 51)
+    personal = build(:personal, mothers_name: 'a' * 51)
     expect(personal).not_to be_valid
   end
 
   it "fathers name should be present" do
-    personal = FactoryGirl.build(:personal, fathers_name: '')
+    personal = build(:personal, fathers_name: '')
     expect(personal).not_to be_valid
   end
 
   it "fathers name should not be too long" do
-    personal = FactoryGirl.build(:personal, fathers_name: 'a' * 51)
+    personal = build(:personal, fathers_name: 'a' * 51)
     expect(personal).not_to be_valid
   end
 end
