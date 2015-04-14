c3f84b6e93935dbae899a4b325150e3bda207667
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-14 10:13:30 +0530

    Address factory

---
+++ spec/factories/address.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :address do
+    line1 'Sample Address line 1'
+    line2 'Sample Address line 2'
+    city_village 'sample city'
+    district 'sample district'
+    state 'sample state'
+    country 'country'
+    pin 808080
+    phone '033 25014532'
+    mobile '+91-8951458370'
+  end
+end
