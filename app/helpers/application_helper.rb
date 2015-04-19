1f3536db3e0297ff31e9131c1c7671087bcf5e1c
Sourav Moitra <astrosourav@gmail.com>
2015-04-19 13:46:35 +0530

    application title added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+
+  def main_title(title)
+    base_title = "Admission"
+    if title.empty?
+      base_title
+    else
+      "#{title} | #{base_title}"
+    end
+  end
 end
