d80a3076070e978fb1608b91664b1d8d1546aa5c
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-19 21:59:00 +0530

    candidate subject validation and refactoring

--- app/models/candidate_stream.rb
+++ app/models/candidate_stream.rb
@@ -6,12 +6,12 @@ class CandidateStream < ActiveRecord::Base
 
   validate :check_marks
   validates :stream_id, presence: true, numericality: { only_intger: true }
-  
+
   def check_marks
     caste = self.candidate.personal.caste.gsub("-", "").downcase
     self.candidate_subjects.each do |cs|
       required_marks = cs.required_subject.castewise_mark.read_attribute(caste)
-      errors.add(:stream_id, "Your marks is less than required marks in #{cs.required_subject.subject_name}") if required_marks > cs.marks
+      errors.add(:stream_id, "Your marks is less than required marks in #{cs.required_subject.subject_name}") if !required_marks.nil? and required_marks > cs.marks
     end
   end
 end
--- app/models/required_subject.rb
+++ app/models/required_subject.rb
@@ -4,6 +4,8 @@ class RequiredSubject < ActiveRecord::Base
   has_many :candidate_subject
   accepts_nested_attributes_for :castewise_mark
 
+  validates :subject_name, presence: true, length: { maximum: 20 }
+
   def to_s
     self.subject_name
   end
