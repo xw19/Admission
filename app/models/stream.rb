0e8f742409a7ac1373b8513aeee28413c01aa894
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-19 21:42:57 +0530

    stream dont accept required subject if subject_name is not given

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -1,5 +1,5 @@
 class Stream < ActiveRecord::Base
   has_many :required_subjects, dependent: :destroy
   has_many :candidate_streams
-  accepts_nested_attributes_for :required_subjects
+  accepts_nested_attributes_for :required_subjects, reject_if: lambda {|attributes| attributes['subject_name'].blank?}
 end
