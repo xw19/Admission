f32c2e58e6056cc04a7503175f560c0d17ea6bd0
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-19 21:45:54 +0530

    stream name validation

--- app/models/stream.rb
+++ app/models/stream.rb
@@ -2,4 +2,6 @@ class Stream < ActiveRecord::Base
   has_many :required_subjects, dependent: :destroy
   has_many :candidate_streams
   accepts_nested_attributes_for :required_subjects, reject_if: lambda {|attributes| attributes['subject_name'].blank?}
+
+  validates :name, presence: true, length: { maximum: 20 }
 end
