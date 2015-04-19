f54597493d4cd9b16742d2ea7190c0378e2bd89b
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-19 21:13:33 +0530

    Qualification validations added

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,4 +1,11 @@
 class Qualification < ActiveRecord::Base
   belongs_to :candidate
   mount_uploader :certificate, CertificateUploader
+
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :description, length: { maximum: 50 }
+  validates :certificate, presence: true
+  validates :points, presence: true, numericality: { only_integer: true }
+  validates :year, presence: true, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }
+  validates :issuing_authority, presence: true, length: { maximum: 20 }
 end
