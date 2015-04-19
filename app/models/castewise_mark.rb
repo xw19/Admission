07a3aa2331a1eb0fef11e0174acce8b6e2dfa41f
Sourav Moitra <sourav.moitr@gmail.com>
2015-04-19 22:06:55 +0530

    castewise marks validations

--- app/models/castewise_mark.rb
+++ app/models/castewise_mark.rb
@@ -1,3 +1,10 @@
 class CastewiseMark < ActiveRecord::Base
   belongs_to :required_subject
+
+  validates :gen, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+  validates :sc, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+  validates :st, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+  validates :obca, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+  validates :obcb, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 100 }
+
 end
--- app/models/required_subject.rb
+++ app/models/required_subject.rb
@@ -3,6 +3,7 @@ class RequiredSubject < ActiveRecord::Base
   has_one :castewise_mark, dependent: :destroy
   has_many :candidate_subject
   accepts_nested_attributes_for :castewise_mark
+  validates_associated :castewise_mark
 
   validates :subject_name, presence: true, length: { maximum: 20 }
 
